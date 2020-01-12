908e088a5a576874e48fc4a97261ebfde44c2ab6
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2020-01-12 14:10:42 +0100

    Remove some comments and added licensing info and original developer copyright notice

--- AshtonsDataDictionary.py
+++ AshtonsDataDictionary.py
@@ -1,7 +1,20 @@
-# -*- coding: utf-8 -*-
-# MySQL Workbench module
-# <description>
-# Written in MySQL Workbench 6.3.10
+#-*- coding: utf-8 -*-
+'''
+MySQLDataDictionary: documents a MySQL model as html
+Copyright (C) 2018  Ashton Lamont
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.'''
 
 from wb import *
 import grt
@@ -9,12 +22,6 @@ from mforms import Utilities, FileChooser
 import mforms
 import datetime
 import os
-
-#ModuleInfo = DefineModule(name="DDReport", author="Ashton Lamont", version="1.0", description="Data Dictionary in HTML format")
-#@ModuleInfo.plugin("rsn86.DBDocPy.DataDicthtmlReport", caption="Data Dictionary in HTML format", description="Data Dictionary report in HTML format", input=[wbinputs.currentCatalog()], pluginMenu="Model")
-#@ModuleInfo.export(grt.INT, grt.classes.db_Catalog)
-#ModuleInfo = DefineModule(AshtonDataDictionary)
-#@ModuleInfo.export()
 
 filePath=""
 newPath=""
