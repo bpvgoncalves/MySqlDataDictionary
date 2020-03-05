c6de6f72c6d6c9bf5badc5ef011e91f2d3c29a10
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2020-03-05 22:23:03 +0100

    delete unused imports

--- MysqlDataDictionary.py
+++ MysqlDataDictionary.py
@@ -18,10 +18,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.'''
 
 import os
-import datetime
 from wb import *
 import grt
-import mforms
 from mforms import Utilities, FileChooser
 
 # Global vars
